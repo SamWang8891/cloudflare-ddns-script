eb3a70b63e9ab2f87956a8196b6d5c8fd466f8a8
SamWang8891 <g348.8891@gmail.com>
2024-10-24 09:26:05 +0800

    Upload the script

---
+++ config.sh
@@ -0,0 +1,20 @@
+###You need to modify the following variables to match your environment
+
+## Specity the DNS A record.
+#Eg. your.domain.tld
+record=""
+
+##Cloudflare Zone ID
+zone_id=""
+
+##Cloudflare Zone API Token
+token=""
+
+##Cloudflare Email
+email=""
+
+##Whether to use proxy, true or false
+proxy="true"
+
+##TTL
+ttl="60"
